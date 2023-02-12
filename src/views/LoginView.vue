d9b1a223d52ada53643935ebd1bd64e8fffabf4a
thiagopaes.dsn.erp <thiagopaes1993@gmail.com>
2023-02-12 17:07:27 -0300

    💄 Nova tela de login e ícone

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="container">
-    <v-card class="pa-5">
-      <v-card-title>
-        <span>EZmail</span>
+    <v-card width="330px" elevation="10" class="pa-5">
+      <v-card-title class="d-flex justify-center mb-3">
+        <h3>Login</h3>
       </v-card-title>
       <v-card-text>
         <v-form ref="form" v-model="valid" lazy-validation>
           <v-text-field
+            outlined
             v-model="email"
             :rules="emailRules"
             label="Email"
-            :loading="loading"
-            single-line
+            class="mb-2"
             required
           ></v-text-field>
 
           <v-text-field
+            outlined
             v-model="password"
+            class=""
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             :rules="passwordRules"
             label="Password"
-            :loading="loading"
-            single-line
             :type="showPassword ? 'text' : 'password'"
             @click:append="showPassword = !showPassword"
             required
@@ -29,23 +29,23 @@
 
           <v-checkbox
             v-model="rememberPassword"
-            class="pa-0 ma-0"
+            class="pa-0 ma-0 mb-2"
             label="Lembrar-me"
             color="primary"
-            value="red"
             hide-details
           ></v-checkbox>
         </v-form>
       </v-card-text>
-
       <v-card-actions class="d-flex justify-center align-center">
         <v-btn
           color="primary"
-          class="btn-login"
+          class="pa-5 mb-5"
+          width="100%"
           :loading="loading"
+          :disabled="!valid"
           @click="login"
         >
-          Login
+          Entrar
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -107,20 +107,5 @@ export default {
   justify-content: center;
   height: 100vh;
   width: 100vw;
-}
-
-.card {
-  padding: 50px;
-  background-color: white;
-  border-radius: 10px;
-  width: 400px;
-  height: 450px;
-  text-align: center;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px,
-    rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
-}
-
-.btn-login {
-  text-align: center;
 }
 </style>
